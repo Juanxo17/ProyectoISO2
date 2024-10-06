c7fca08a770f95b8d37aad3bb2784071380c8c09
Juan Plata <plata.barbosa@gmail.com>
2024-10-06 18:30:33 -0500

    Added .env and chromadb server into main

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
 # app/main.py
 from fastapi import FastAPI
 from app.api import routers
+import chromadb
+
+# Inicia el cliente ChromaDB
+client = chromadb.Client()
+print("ChromaDB está corriendo!")
 
 app = FastAPI()
-
 app.include_router(routers.rag_router)
 
 if __name__ == "__main__":
     import uvicorn
-
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    uvicorn.run(app, host="127.0.0.1", port=8001)
